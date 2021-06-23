ebed22319a424dd1e13095a73233541ee8ab808f
harsimar17290 <harsimar17290@iiitd.ac.in>
2021-06-23 11:57:42 +0530

    Change export

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # amfi-database-creator
-This package can be used for creating / updating a MongoDB database with up-to-date information about Indian Mutual Funds fetched from the AMFI portal. 
+
+This package can be used for creating / updating a MongoDB database with up-to-date information about Indian Mutual Funds fetched from the AMFI portal.
 
 ## Usage
+
 ```javascript
-const DbCreator = require('amfi-database-creator');
+(async () => {
 
-const dbUrl = 'mongodb://localhost:27017/amfi-db';
+  const navDB = await (require("amfi-database-creator")).getInstance(Config.databaseUrl);
+  const MutualFund = navDB.MutualFund;
 
-const dbCreator = new DbCreator(dbUrl);
+  await navDB.updateDB();
 
-dbCreator.updateDB();
+  const fund = await MutualFund.findOne();
+
+})();
 ```
--- main.js
+++ main.js
@@ -1,42 +1,47 @@
 const nav = require('india-mutual-fund-info');
 const mongoose = require('mongoose');
 
-const MetaModel = mongoose.model('Meta', new mongoose.Schema({ lastUpdated: Date }));
-const MutualFundSchema = new mongoose.Schema({
-    'Scheme Code': { type: String, index: true, unique: true },
-    'ISIN Div Payout/ISIN Growth': String,
-    'ISIN Div Reinvestment': String,
-    'Scheme Name': String,
-    'AMC Name': String,
-    'Scheme Type': String,
-    'NAVs': [{
-        '_id': String,
-        'NAV': String
-    }]
-}, { strict: 'false' });
-MutualFundSchema.index({'Scheme Name':'text'});
-const MutualFundModel = mongoose.model('MutualFund', MutualFundSchema);
-
-
 class DbCreator {
-
     constructor(dbUrl) {
+        this.MetaModel = mongoose.model('Meta', new mongoose.Schema({ lastUpdated: Date }));
+        this.MutualFundSchema = new mongoose.Schema({
+            'Scheme Code': { type: String, index: true, unique: true },
+            'ISIN Div Payout/ISIN Growth': String,
+            'ISIN Div Reinvestment': String,
+            'Scheme Name': String,
+            'AMC Name': String,
+            'Scheme Type': String,
+            'NAVs': [{
+                '_id': String,
+                'NAV': String
+            }]
+        }, { strict: 'false' });
+        this.MutualFundSchema.index({ 'Scheme Name': 'text' });
+        this.MutualFund = mongoose.model('MutualFund', this.MutualFundSchema);
         this.dbUrl = dbUrl;
         this.ms1Day = 24 * 60 * 60 * 1000;
+    }
+
+    static async getInstance(dbUrl) {
+        if (!this.instance) {
+            await mongoose.connect(dbUrl);
+            this.instance = new DbCreator(dbUrl);
+        }
+        return this.instance;
     }
 
     dateEquals = (d1, d2) => { return d1.getDate() == d2.getDate() && d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear() };
     dateStr = date => `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 
     async createMeta() {
-        var meta = await MetaModel.create({
+        var meta = await this.MetaModel.create({
             lastUpdated: '2006-03-31T00:00:00.000Z'
         });
         return (meta);
     }
 
     async getLastUpdated() {
-        const metas = await MetaModel.find();
+        const metas = await this.MetaModel.find();
         var meta = metas[0];
         if (meta === undefined)
             meta = await this.createMeta();
@@ -56,7 +61,7 @@ class DbCreator {
         var count = 0;
         for (let item of uniques) {
             const { NAVs, ...info } = item;
-            await MutualFundModel.updateOne({ 'Scheme Code': item['Scheme Code'] }, {
+            await this.MutualFund.updateOne({ 'Scheme Code': item['Scheme Code'] }, {
                 ...info,
                 $addToSet: {
                     'NAVs': { $each: NAVs }
@@ -67,7 +72,7 @@ class DbCreator {
         const date = updateDate.getDate();
         const month = updateDate.getMonth() + 1;
         const year = updateDate.getFullYear();
-        await MetaModel.updateOne({}, {
+        await this.MetaModel.updateOne({}, {
             lastUpdated: `${year}-${month < 10 ? `0${month}` : month}-${date < 10 ? `0${date}` : date}T00:00:00.000Z`
         });
         return count;
@@ -83,27 +88,26 @@ class DbCreator {
         console.log(`Updated period ${this.dateStr(from)} - ${this.dateStr(to)}. ${count} documents updated in transaction.`);
     }
 
-    async updateNavAll(){
+    async updateNavAll() {
         const now = new Date(Date.now());
         const data = await nav.today();
-        const count = await this.persistData(data,now);
+        const count = await this.persistData(data, now);
         console.log(`Updated NAVs for ${this.dateStr(now)}. ${count} documents updated in transaction.`);
     }
 
     async updateDB() {
-        this.conn = await mongoose.connect(this.dbUrl);
         var lastUpdated = await this.getLastUpdated();
         console.log(`DB last updated on ${this.dateStr(lastUpdated)}`);
         var now = new Date(Date.now());
-        if(this.dateEquals(lastUpdated,now)){
+        if (this.dateEquals(lastUpdated, now)) {
             await this.updateNavAll();
         }
         while (!this.dateEquals(lastUpdated, now)) {
             await this.update90();
             lastUpdated = await this.getLastUpdated();
         }
-        this.conn.disconnect();
     }
+
 }
 
 module.exports = DbCreator;
